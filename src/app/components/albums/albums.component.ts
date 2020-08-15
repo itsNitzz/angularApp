b10b97631e48f0cc7f27dfe5d8a80db8c482984e
ranvir jha <maxtrick.io@outlook.com>
2020-08-15 12:09:36 +0530

    ~ < + g_services

--- src/app/components/albums/albums.component.ts
+++ src/app/components/albums/albums.component.ts
@@ -16,5 +16,4 @@ export class AlbumsComponent implements OnInit {
   ngOnInit(): void {
     this.albums = this.photosService.getAlbums();
   }
-
 }
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,12 @@
+import { AlbumsComponent } from './../albums/albums.component';
 import { Component, OnInit, ViewChild, ViewContainerRef, ComponentRef, ComponentFactoryResolver, Inject } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {UserLogComponent} from '../../authentication/user-log.component';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -8,7 +14,14 @@ import {UserLogComponent} from '../../authentication/user-log.component';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  constructor(public dialog: MatDialog) {
+  @ViewChild('dynamic', {read: ViewContainerRef}) target: ViewContainerRef;
+  private componentRef: ComponentRef<any>;
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+  constructor(public dialog: MatDialog, private componentFactoryResolver: ComponentFactoryResolver) {
    }
 
    ngOnInit() { }
@@ -18,5 +31,10 @@ export class DashboardComponent implements OnInit {
       width: '850px',
       height: '525px'
     });
+  }
+
+  addElement(){
+    let childComponent = this.componentFactoryResolver.resolveComponentFactory(AlbumsComponent);
+    this.componentRef = this.target.createComponent(childComponent);
   }
 }
